1674f1bc1621b371cff24e7c7052a5133470d301
Isaac Oluwafemi Ogunniyi <isaacogunniyi1@gmail.com>
2023-05-22 20:17:24 +0000

    combined training and usage

--- app.py
+++ app.py
@@ -4,13 +4,30 @@ import numpy as np
 from sklearn.metrics import f1_score
 import sklearn
 import pickle
-import xgboost
 
 
+@st.cache_data
 def train_model():
+    test = pd.read_csv('test.csv')
     data = pd.read_csv('better_train.csv')
-    model =pickle.load(open('bl_untrained_model.pkl','rb'))
-    preprocessor = pickle.load(open('process.pkl','rb'))
+    
+    X = data.drop('is_fraud',axis=1)
+    y = data['is_fraud']
+    X_train,X_test,y_train,y_test= sklearn.model_selection.train_test_split(X,
+    y,test_size=0.3,random_state=2)
+    return (X_test,test)
+
+@st.cache_data
+def predict_test(pdata):
+    model =pickle.load(open('b_trained_model.pkl','rb'))
+    pred = model.predict(pdata)
+    
+    return (pred)
+
+@st.cache_data
+def training_model():
+    data = pd.read_csv('better_train.csv')
+    model =pickle.load(open('untrained_model.pkl','rb'))
     X = data.drop('is_fraud',axis=1)
     y = data['is_fraud']
     X_train,X_test,y_train,y_test= sklearn.model_selection.train_test_split(X,
@@ -20,13 +37,22 @@ def train_model():
     te_pred = model.predict(X_test)
     train_score = f1_score(y_train,tr_pred)
     test_score = f1_score(y_test,te_pred)
-    return (preprocessor,model,train_score,test_score)
-
-
+    return (preprocessor,model,train_score,test_score)  
+    
+@st.cache_data
+def prepare(test):
+    preprocessor = pickle.load(open('process.pkl','rb'))
+    X = test.drop('is_fraud',axis=1)
+    data = pd.DataFrame(preprocessor.transform(X))
+    
+    return (data)
 
 def main():
-
     
+
+   
+
+
     st.sidebar.header('Dataset to use')
     page = st.sidebar.selectbox("Data Input", ['Default','User Upload'])
     
@@ -35,20 +61,48 @@ def main():
     st.title('Credit Card Fraud Predictor')
     
     with st.spinner("Training the model... Please wait."):
-        preprocessor,model,trainscore,testscore = train_model()
+        model,trainscore,testscore = training_model()
     st.write('train score:', trainscore)
     st.write('test score:', testscore)
+    
+    with st.spinner("Unpacking the model... Please wait."):
+        X_test,test = train_model()
 
-    model_filename = "b_trained_model.pkl"
-    with open(model_filename, "wb") as f:
-        pickle.dump(model, f)
+    if page == 'Default':
+        st.header('Predicting Default Test Data')
+        st.subheader('Dataset Preview')
+        
+        test
 
-    # Provide a download link for the trained model
-    st.download_button(
-        label="Download Trained Model",
-        data=open(model_filename, "rb").read(),
-        file_name=model_filename
-    )
+        if st.button("Predict on unseen test data above"):
+            with st.spinner("Preparing data... Please wait."):
+                pdata = prepare(test)
+                
+            with st.spinner("Predicting... Please wait."):
+                predic = predict_test(pdata)
+
+            st.subheader('Results')
+            predic
+
+            f1= f1_score(test['is_fraud'],predic)
+            st.subheader('F1 Score')
+            f1
+        st.write('''***''')
+
+
+    elif page == 'User Upload':
+        st.header('Predicting on Uploaded Data')
+        u_data = st.file_uploader('The transactions on which you want to predict',type='csv')
+        if u_data is not None:
+            data = pd.read_csv(u_data,index_col = 'ID')
+
+            st.subheader('Dataset Preview')
+            data
+
+ 
+
+        else:
+            st.write('No dataset Uploaded')
     
         
 
