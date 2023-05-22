00173cb746e58bf33ed3cab8470b77e79f6bc376
Isaac Oluwafemi Ogunniyi <isaacogunniyi1@gmail.com>
2023-05-22 20:54:20 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,39 +4,32 @@ import numpy as np
 from sklearn.metrics import f1_score
 import sklearn
 import pickle
+import xgboost
 
 
 @st.cache_data
 def train_model():
     test = pd.read_csv('test.csv')
     data = pd.read_csv('better_train.csv')
+    model =pickle.load(open('b_trained_model.pkl','rb'))
     
     X = data.drop('is_fraud',axis=1)
     y = data['is_fraud']
     X_train,X_test,y_train,y_test= sklearn.model_selection.train_test_split(X,
     y,test_size=0.3,random_state=2)
-    return (X_test,test)
-
-@st.cache_data
-def predict_test(pdata,model):
-    pred = model.predict(pdata)
-    
-    return (pred)
-
-@st.cache_data
-def training_model():
-    data = pd.read_csv('better_train.csv')
-    model =pickle.load(open('untrained_model.pkl','rb'))
-    X = data.drop('is_fraud',axis=1)
-    y = data['is_fraud']
-    X_train,X_test,y_train,y_test= sklearn.model_selection.train_test_split(X,
-    y,test_size=0.3,random_state=2)
-    model = model.fit(X_train,y_train)
     tr_pred = model.predict(X_train)
     te_pred = model.predict(X_test)
     train_score = f1_score(y_train,tr_pred)
     test_score = f1_score(y_test,te_pred)
-    return (model,train_score,test_score)  
+    return (X_test,test,model,train_score,test_score)
+
+@st.cache_data
+def predict_test(pdata):
+    model =pickle.load(open('b_trained_model.pkl','rb'))
+    pred = model.predict(pdata)
+    
+    return (pred)
+    
     
 @st.cache_data
 def prepare(test):
@@ -48,10 +41,6 @@ def prepare(test):
 
 def main():
     
-
-   
-
-
     st.sidebar.header('Dataset to use')
     page = st.sidebar.selectbox("Data Input", ['Default','User Upload'])
     
@@ -59,13 +48,10 @@ def main():
     
     st.title('Credit Card Fraud Predictor')
     
-    with st.spinner("Training the model... Please wait."):
-        model,trainscore,testscore = training_model()
-    st.write('train score:', trainscore)
-    st.write('test score:', testscore)
-    
     with st.spinner("Unpacking the model... Please wait."):
-        X_test,test = train_model()
+        X_test,test,model,trainscore,testscore = train_model()
+    st.write('train f1 score:', trainscore)
+    st.write('test f1 score:', testscore)
 
     if page == 'Default':
         st.header('Predicting Default Test Data')
@@ -78,14 +64,14 @@ def main():
                 pdata = prepare(test)
                 
             with st.spinner("Predicting... Please wait."):
-                predic = predict_test(pdata,model)
+                predic = predict_test(pdata)
 
             st.subheader('Results')
             predic
 
-            f1= f1_score(test['is_fraud'],predic)
-            st.subheader('F1 Score')
-            f1
+            #f1= f1_score(test['is_fraud'],predic)
+            #st.subheader('F1 Score')
+            #f1
         st.write('''***''')
 
 
