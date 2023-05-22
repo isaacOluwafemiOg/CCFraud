ba8fa70f0eefc991d8ba76c2cef4c80b09275d8e
Isaac Oluwafemi Ogunniyi <isaacogunniyi1@gmail.com>
2023-05-22 08:25:57 +0000

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,12 @@ import sklearn
 import pickle
 import xgboost
 
-def train_model(model,data):
+
+@st.cache_data
+def train_model():
+    data = pd.read_csv('train.csv')
+    model =pickle.load(open('ccFraud.pkl','rb'))
+    preprocessor = pickle.load(open('process.pkl','rb'))
     X = data.drop('is_fraud',axis=1)
     y = data['is_fraud']
     X_train,X_test,y_train,y_test= sklearn.model_selection.train_test_split(X,
@@ -16,7 +21,7 @@ def train_model(model,data):
     te_pred = model.predict(X_test)
     train_score = f1_score(y_train,tr_pred)
     test_score = f1_score(y_test,te_pred)
-    return (model,train_score,test_score)
+    return (preprocessor,model,train_score,test_score)
 
 
 
@@ -25,50 +30,27 @@ def main():
     
     st.sidebar.header('Dataset to use')
     page = st.sidebar.selectbox("Data Input", ['Default','User Upload'])
-    model =pickle.load(open('ccFraud.pkl','rb'))
-    preprocessor = pickle.load(open('process.pkl','rb'))
-    traincsv = pd.read_csv('train.csv')
-
-    model,trainscore,testscore = train_model(model,traincsv)
+    
+    
+    
+    st.title('Credit Card Fraud Predictor')
+    
+    with st.spinner("Training the model... Please wait."):
+        preprocessor,model,trainscore,testscore = train_model()
     st.write('train score:', trainscore)
     st.write('test score:', testscore)
 
-    if page == 'Default':
-        st.title('Predicting Default Test Data')
-        st.subheader('Dataset Preview')
-        test = pd.read_csv('test.csv')
-        test
+    model_filename = "trained_model.pkl"
+    with open(model_filename, "wb") as f:
+        pickle.dump(model, f)
 
-        X = test.drop('is_fraud',axis=1)
-        data = pd.DataFrame(preprocessor.fit_transform(X))
-        prediction=model.predict(data)
-
-        st.subheader('Results')
-        prediction
-
-        f1= f1_score(test['is_fraud'],prediction)
-        st.subheader('F1 Score')
-        f1
-        st.write('''***''')
-
-
-    elif page == 'User Upload':
-        st.title('Predicting on Uploaded Data')
-        u_data = st.file_uploader('The transactions on which you want to predict',type='csv')
-        if u_data is not None:
-            data = pd.read_csv(u_data,index_col = 'ID')
-
-            st.subheader('Dataset Preview')
-            data
-
-            testh = pd.DataFrame(preprocessor.fit_transform(data))
-            prediction=model.predict(testh)
-
-            st.subheader('Results')
-            prediction
-
-        else:
-            st.write('No dataset Uploaded')
+    # Provide a download link for the trained model
+    st.download_button(
+        label="Download Trained Model",
+        data=open(model_filename, "rb").read(),
+        file_name=model_filename
+    )
+    
         
 
 
